6c5d0126fa3b672f28c6d0a86ab353d7abf33f65
Luis Fernando Camacho <20253ds128@utez.edu.mx>
2026-03-15 19:28:59 -0600

    Limpia template de JavaFX y crea estructura base del proyecto

--- catalogo-libros-javafx/src/main/java/com/utez/catalogolibros/catalogolibrosjavafx/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.utez.catalogolibros.catalogolibrosjavafx;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
