da19707131d6d2903d4770ccb06b7769e62a7f39
Luis Fernando Camacho <20253ds128@utez.edu.mx>
2026-03-15 19:55:09 -0600

    Eliminación de module-info para simplificar la configuración de JavaFX

--- catalogo-libros-javafx/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.utez.catalogolibros.catalogolibrosjavafx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.utez.catalogolibros.catalogolibrosjavafx to javafx.fxml;
-    exports com.utez.catalogolibros.catalogolibrosjavafx;
-}
