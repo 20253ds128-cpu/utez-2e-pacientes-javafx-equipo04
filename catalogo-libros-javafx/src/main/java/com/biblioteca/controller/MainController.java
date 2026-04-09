6a69ec82ebed8bfd5ff026bb73e3f64e294902e1
Emiliano_2E <20253ds125@utez.edu.mx>
2026-04-09 00:43:31 -0600

    codigo de mainController terminado

--- catalogo-libros-javafx/src/main/java/com/biblioteca/controller/MainController.java
+++ catalogo-libros-javafx/src/main/java/com/biblioteca/controller/MainController.java
@@ -1,4 +1,41 @@
 package com.biblioteca.controller;
 
-public class MainController {
+
+import com.biblioteca.services.LibroServices;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainController extends Application {
+
+    private static LibroServices libroServices;
+
+    @Override
+    public void start (Stage primaryStage) throws IOException{
+        libroServices = new LibroServices();
+
+        FXMLLoader loder = new FXMLLoader(getClass().getResource("/fxml/libro.fxml"));
+
+        Scene scene = new Scene(loder.load(), 900, 600);
+
+        scene.getStylesheets().add(getClass().getResource("/css/style.css").toExternalForm());
+
+
+        primaryStage.setTitle("Biblioteca Escolar - Catalogo de libros ");
+        primaryStage.setScene(scene);
+        primaryStage.setMinWidth(800);
+        primaryStage.setMinHeight(500);
+        primaryStage.show();
+    }
+
+    public static LibroServices getLibroServices(){
+        return libroServices;
+    }
+
+    public static void main (String[] args){
+        launch(args);
+    }
 }
